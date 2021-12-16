35117927758f4e94726fc7da75ffa49e7c39c8d5
Utsab Pokharel <pokharelutsab567@gmail.com>
2021-12-16 17:49:33 +0545

    changes in frontend

--- resources/views/Admin/users/create.blade.php
+++ resources/views/Admin/users/create.blade.php
@@ -16,7 +16,7 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                    <h2>Create New User</h2>
+                    <h2 style="margin-left: 30px;">Create New User</h2>
                 </div>
                 <div class="pull-right">
                     <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
@@ -40,67 +40,70 @@
 
         {!! Form::open(array('route' => 'users.store','method'=>'POST','enctype'=>'multipart/form-data')) !!}
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <span class="required"> * </span>
-                    {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+            <div class="card-create">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong class="form-label">Name:</strong>
+                        <span class="required"> * </span>
+                        {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Email:</strong>
-                    <span class="required"> * </span>
-                    {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong class="form-label">Email:</strong>
+                        <span class="required"> * </span>
+                        {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Password:</strong>
-                    <span class="required"> * </span>
-                    {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong class="form-label">Password:</strong>
+                        <span class="required"> * </span>
+                        {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Confirm Password:</strong>
-                    <span class="required"> * </span>
-                    {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' =>
-                    'form-control'))
-                    !!}
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong class="form-label">Confirm Password:</strong>
+                        <span class="required"> * </span>
+                        {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' =>
+                        'form-control'))
+                        !!}
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Role:</strong>
-                    <span class="required"> * </span>
-                    {!! Form::select('roles[]', $roles,[], array('class' =>
-                    'select2', 'form-control' ,'select2-multiple','multiple'))
-                    !!}
-                    {{-- <select class="select2 form-control select2-multiple" multiple="multiple" multiple
-                        placeholder="Choose ..." name="roles[]">
-                        @foreach ($roles as $roles)
-                        <option value="{{$roles,[]}}" selected>{{$roles}}</option>
-                    @endforeach
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong class="form-label">Role:</strong>
+                        <span class="required"> * </span>
+                        {!! Form::select('roles[]', $roles,[], array('class' =>
+                        'select2', 'form-control' ,'select2-multiple','multiple'))
+                        !!}
+                        {{-- <select class="select2 form-control select2-multiple" multiple="multiple" multiple
+                            placeholder="Choose ..." name="roles[]">
+                            @foreach ($roles as $roles)
+                            <option value="{{$roles,[]}}" selected>{{$roles}}</option>
+                            @endforeach
 
-                    </select> --}}
+                        </select> --}}
+                    </div>
+                </div>
+                {{-- <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>User Image:</strong>
+                        {!! Form::file('user_image', array('placeholder' => 'User Image','class' =>
+                        'form-control','id'=>"img_cert" ,'onchange'=>"imgCert(this);",'enctype'=>'multipart/form-data'))
+                        !!}
+                        <img src="{{asset('assets/images/users/profile.png')}}" alt="image" id="img-preview"
+                            width="150px">
+                    </div>
+                </div> --}}
+                <div class="col-xs-12 col-sm-12 col-md-1">
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </div>
-            {{-- <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>User Image:</strong>
-                    {!! Form::file('user_image', array('placeholder' => 'User Image','class' =>
-                    'form-control','id'=>"img_cert" ,'onchange'=>"imgCert(this);",'enctype'=>'multipart/form-data'))
-                    !!}
-                    <img src="{{asset('assets/images/users/profile.png')}}" alt="image" id="img-preview" width="150px">
         </div>
-    </div> --}}
-    <div class="col-xs-12 col-sm-12 col-md-1">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        {!! Form::close() !!}
     </div>
-</div>
-{!! Form::close() !!}
-</div>
 </div>
 
 @endsection
